b38a9a1e410a4094320abfb6ca5fa3d747962a3b
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2024-08-10 10:33:15 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -238,8 +238,7 @@ app.get('/transactions', async (req, res) => {
 
 
 // Start the server
-app.listen(3000, '0.0.0.0',(s) => {
-    const ipAddress = getNetworkIP();
-    console.log(`Server started on http://${ipAddress}:3000`);
+app.listen(3000,() => {
+    console.log(`Server started on http://localhost:3000`);
 });
 mongoose.connect(`mongodb://127.0.0.1:27017/solana`)
